bec4dc6cef7bf184d124a676cc82e2823db8326f
drbrunompedroso <drbrunompedroso@gmail.com>
2022-09-09 22:07:55 -0300

    Aplicação com try-catch

---
+++ TratativaERRO_ValorNumerico/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TratativaERRO_app
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            double Value1,
+                   Value2,
+                   calc;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.SetWindowSize(70, 30);
+            Console.Title = "EXEMPLO TRATATIVA DE ERRO";
+
+            Console.WriteLine("Aplicação para Tratativa de ERROS:");
+            
+            try
+            {
+                Console.WriteLine("\nEntre com o Valor 1:");
+                Value1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nEntre com o Valo 2:");
+                Value2 = Convert.ToDouble(Console.ReadLine());
+                calc = Value1 + Value2;
+                Console.WriteLine("\nResultado: {0}", calc);
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("\nNão é possível solucionar o cálculo com os valores informados "); 
+            }
+            finally
+            {
+                Console.WriteLine("\nCÁCULO - Soma de valores");
+            }
+            Console.ReadKey();
+        }
+    }
+}
